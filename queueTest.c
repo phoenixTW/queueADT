bb9acef3dc49e857a17344da3321017637e6ae15
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-02-06 12:49:21 +0530

    3 tests are written for createQueue() method. The queue will have a list and null front and rear.

--- queueTest.c
+++ queueTest.c
@@ -7,3 +7,21 @@ void test_createQueue_should_return_an_empty_queue () {
 	assertEqual(q.list->count, 0);
 	free(q);
 }
+
+void test_createQueue_should_return_an_empty_queue_with_null_front () {
+	Queue q;
+	q = createQueue();
+	assertEqual(q.list->count, 0);
+	assertEqual(q.list->head, q.front);
+	assertEqual(q.front, 0x0);
+	free(q);
+}
+
+void test_createQueue_should_return_an_empty_queue_with_null_rear () {
+	Queue q;
+	q = createQueue();
+	assertEqual(q.list->count, 0);
+	assertEqual(q.list->tail, q.rear);
+	assertEqual(q.rear, 0x0);
+	free(q);
+}
